28c47ee01409b17a3efe9d03d5813fdc49d0a1b1
xpa <xpadev@gmail.com>
2023-03-26 14:38:59 +0900

    fix: send join room response to all user

--- server.py
+++ server.py
@@ -1,35 +1,35 @@
 import logging
 import json
-import uuid #print(uuid.uuid1())
+import uuid  # print(uuid.uuid1())
 from collections import defaultdict
 
 from websocket_server import WebsocketServer
 
-#map<string,vector<strinng>>; roomIdに対して所属するメンバーのuseridを保持する。
+# map<string,vector<strinng>>; roomIdに対して所属するメンバーのuseridを保持する。
 room_member_id = defaultdict(list)
 
-#map<string,vector<strinng>>; roomIdに対して所属するメンバーのusernameを保持する。
+# map<string,vector<strinng>>; roomIdに対して所属するメンバーのusernameを保持する。
 room_member_name = defaultdict(list)
 
-#部屋のIdを持つ配列 
-room_name = [] 
+# 部屋のIdを持つ配列 
+room_name = []
 
-#map<string,string> ;userid->username
+# map<string,string> ;userid->username
 name_id = {}
 
-#map<string,bool> ;useiid->推測|コーディング終了したか。
+# map<string,bool> ;useiid->推測|コーディング終了したか。
 id_bool = {}
 
 # 発行した user_id と token を記録
 user_token = dict()
 
-#codeを記録する配列
+# codeを記録する配列
 ans_code = defaultdict(list)
 
-#answerを記録する配列
+# answerを記録する配列
 ans_str = defaultdict(list)
 
-#問題
+# 問題
 text = [
     "長さNの整数列A = (A1,A2,...AN)が与えられます。Aから偶数だけすべて取り出し、もとの順番を保って出力してください。",
     "英小文字からなる文字列Sが与えられます。Sの各文字を英大文字に変換して得られる文字列Tを出力してください。",
@@ -40,21 +40,36 @@ text = [
     "サッカーワールドカップは西暦年を4で割った余りが2である年の6月に行われます。現在がY年の1月であるとき、次のワールドカップはいつ開催されますか？（入力として整数Yが与えられます。）",
 ]
 
-def join_room(req: dict) -> dict:
-    res = {
+
+def join_room(req: dict):
+    on_room_user_list_update = {
         "type": "onRoomUserListUpdate",
         "users": []
     }
-    room_Id = req['roomId']
-    room_member_id[room_Id].append(req['userId'])
-    room_member_name[room_Id].append(req['username'])
-    res['users'].append(
+    room_id = req['roomId']
+    room_member_id[room_id].append(req['userId'])
+    room_member_name[room_id].append(req['username'])
+    on_room_user_list_update['users'].append(
         {
-            "userId": room_member_id[room_Id],
-            "username": room_member_name[room_Id]
+            "userId": room_member_id[room_id],
+            "username": room_member_name[room_id]
         }
     )
-    return res
+
+    join_room_response = {
+        "type": "joinRoomResponse",
+        "roomId": room_id,
+        "userId": req['userId'],
+        "owner": False,
+        "users": [
+            {
+                "userId": room_member_id[room_id],
+                "username": room_member_name[room_id]
+            }
+        ]
+    }
+    return on_room_user_list_update, join_room_response
+
 
 def create_room(req: dict) -> dict:
     res = {
@@ -64,20 +79,21 @@ def create_room(req: dict) -> dict:
         "owner": "<boolean>",
         "users": []
     }
-    room_Id = uuid.uuid4().hex
-    room_member_id[room_Id].append(req['userId'])
-    room_member_name[room_Id].append(req['username'])
-    room_name.append(room_Id)
-    res['roomId'] = room_Id
+    room_id = uuid.uuid4().hex
+    room_member_id[room_id].append(req['userId'])
+    room_member_name[room_id].append(req['username'])
+    room_name.append(room_id)
+    res['roomId'] = room_id
     res['userId'] = req['userId']
     res['owner'] = True
     res['users'].append(
         {
-            "userId": room_member_id[room_Id],
-            "username": room_member_name[room_Id]
+            "userId": room_member_id[room_id],
+            "username": room_member_name[room_id]
         }
     )
     return res
+
 
 def start_game(req: dict) -> dict:
     res = {
@@ -85,17 +101,20 @@ def start_game(req: dict) -> dict:
         "phase": "",
         "data": ""
     }
-    room_Id = req['roomId']
+    room_id = req['roomId']
     siz = len(room_member_name)
     res['phase'] = "coding"
     res['data'] = text[3]
     return res
 
+
 def end_phase(req: dict) -> dict:
     return
 
+
 def open_next_result(req: dict) -> dict:
     return
+
 
 def state_update(req: dict) -> dict:
     res = {
@@ -111,6 +130,7 @@ def state_update(req: dict) -> dict:
         }
     )
     return res
+
 
 def request_user_id(req: dict) -> dict:
     res = {
@@ -141,10 +161,10 @@ def end_game(req: dict) -> dict:
     }
     return res
 
+
 def main():
     def new_client(client: dict, server: WebsocketServer):
         print(f'新規クライアントが ID={client["id"]} で接続しました')
-        server.send_message_to_all('Hey all, a new client has joined us')
         return
 
     def client_left(client: dict, server: WebsocketServer):
@@ -155,41 +175,43 @@ def main():
         print(f'ID={client["id"]} から "{message}" を受信しました')
         data = json.loads(message)
         print(data['type'])
-        if data['type']=="joinRoomRequest":#部屋参加リクエスト
-            response = join_room(data)
-            enc = json.dumps(response, separators=(',',':'))
-            server.send_message(client, enc)
-        elif data['type']=="createRoomRequest":#部屋作成リクエスト
+        if data['type'] == "joinRoomRequest":  # 部屋参加リクエスト
+            on_room_user_list_update, join_room_response = join_room(data)
+            on_room_user_list_update_json = json.dumps(on_room_user_list_update, separators=(',', ':'))
+            server.send_message_to_all(on_room_user_list_update_json)
+            join_room_response_json = json.dumps(join_room_response, separators=(',', ':'))
+            server.send_message(client, join_room_response_json)
+        elif data['type'] == "createRoomRequest":  # 部屋作成リクエスト
             response = create_room(data)
-            enc = json.dumps(response, separators=(',',':'))
+            enc = json.dumps(response, separators=(',', ':'))
             server.send_message(client, enc)
-        elif data['type']=="endPhaseRequest":#状態終了リクエスト
+        elif data['type'] == "endPhaseRequest":  # 状態終了リクエスト
             response = state_update(data)
             flag = True
-            for S in id_bool :
-                if S==False :
+            for S in id_bool:
+                if S == False:
                     flag = True
-            if flag == True :
+            if flag == True:
                 response2 = end_phase(data)
-                enc2 = json.dumps(response2, separators=(',',':'))
+                enc2 = json.dumps(response2, separators=(',', ':'))
                 server.send_message(client, enc2)
-            enc = json.dumps(response, separators=(',',':'))
+            enc = json.dumps(response, separators=(',', ':'))
             server.send_message(client, enc)
-        elif data['type']=="openNextResultRequest":#開示リクエスト
+        elif data['type'] == "openNextResultRequest":  # 開示リクエスト
             response = open_next_result(data)
-            enc = json.dumps(response, separators=(',',':'))
+            enc = json.dumps(response, separators=(',', ':'))
             server.send_message(client, enc)
-        elif data['type']=="startGameRequest":#ゲーム開始リクエスト
+        elif data['type'] == "startGameRequest":  # ゲーム開始リクエスト
             response = start_game(data)
-            enc = json.dumps(response, separators=(',',':'))
+            enc = json.dumps(response, separators=(',', ':'))
             server.send_message(client, enc)
-        elif data['type']=="userIdRequest":#ユーザーIDリクエスト
+        elif data['type'] == "userIdRequest":  # ユーザーIDリクエスト
             response = request_user_id(data)
             enc = json.dumps(response, separators=(',', ':'))
             server.send_message(client, enc)
-        elif data['type']=="gameEndRequest":#ゲーム終了リクエスト
+        elif data['type'] == "gameEndRequest":  # ゲーム終了リクエスト
             response = end_game(data)
-            enc = json.dumps(response, separators=(',',':'))
+            enc = json.dumps(response, separators=(',', ':'))
             server.send_message(client, enc)
 
     server = WebsocketServer(host='127.0.0.1', port=9001, loglevel=logging.DEBUG)
@@ -198,6 +220,7 @@ def main():
     server.set_fn_message_received(message_received)
     server.run_forever()
     return
+
 
 if __name__ == '__main__':
     main()
